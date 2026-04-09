438ad1cc2ee1276576de2f1a6333cf60167d52a8
Luiz Phelipe <luiz.compras1402@gmail.com>
2026-04-09 13:38:13 -0300

    Primeiro Commit, contendo a primeira estrutura de Dags aprendida oficialmente

---
+++ dag-structure.py
@@ -0,0 +1,54 @@
+"""
+DAG Skeleton
+
+- Imports
+- Connections e Variables
+- Default Arguments
+- DAG Definition
+- Task Declaration
+- Task Dependencies
+- DAG Instantiation
+
+"""
+
+# TODO IMPORTS
+
+from datetime import datetime, timedelta
+from airflow.decorators import dag
+from airflow.operations.empty import EmptyOperators
+
+
+# TODO Default arguments
+default_args = {
+    'owner': 'luiz phelipe m. goncalves',
+    'retries': 1,
+    'retry_relay': timedelta(minutes=5)
+}
+
+# TODO DAG definition
+
+@dag(
+    dag_id= 'dag-structure',
+    start_date= datetime(2024,9,30),
+    max_active_runs=1,
+    schedule_interval= timedelta(days=1),
+    catchup=False,
+    default_args=default_args,
+    tags=['first', 'dag']
+
+)
+def init():
+    # TODO task declaration
+
+
+    start = EmptyOperators(task_id='start')
+    end = EmptyOperators(task_id='end')
+
+    # TODO task dependences
+
+    start >> end
+
+    # TODO DAG Instantiation
+
+dag = init()
+
